71af6ea7ada05d5269575dfb27108693c95da69e
adibandla <adityabandla@gmail.com>
2025-09-24 23:59:53 -0400

    Create 9915.py

---
+++ pandas/9915.py
@@ -0,0 +1,40 @@
+# https://platform.stratascratch.com/coding/9915-highest-cost-orders?code_type=2
+# Find the customers with the highest daily total order cost between 2019-02-01 and 2019-05-01. If a customer had more than one order on a certain day, 
+# sum the order costs on a daily basis. Output each customer's first name, total cost of their items, and the date.
+
+# For simplicity, you can assume that every first name in the dataset is unique.
+# Difficulty: Medium
+
+# <customers>
+# id				int64
+# first_name		object
+# last_name			object
+# city				object
+# address			object
+# phone_number		object
+
+# <orders>
+# id				int64
+# cust_id			int64
+# order_date		datetime64[ns]
+# order_details		object
+# total_order_cost	int64
+
+# import your libraries
+import pandas as pd
+
+# glimse data
+customers.head()
+
+# merge tables and filter dates
+customers.merge(
+    orders[orders['order_date'].between('2019-02-01', '2019-05-01')],
+    left_on = 'id',
+    right_on = 'cust_id'
+    ) \
+    .groupby(['first_name', 'order_date'])['total_order_cost'] \
+    .sum() \
+    .reset_index() \
+    .groupby('order_date') \
+    .apply(lambda x: x[x['total_order_cost'] == x['total_order_cost'].max()]) \
+    .reset_index(drop=True)
